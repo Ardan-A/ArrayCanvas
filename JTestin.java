5215d5da1c385a04b7c2df8b664cc332755f51e3
Ardan A <71040066+Ardan-A@users.noreply.github.com>
2022-12-25 13:21:51 -0500

    Change to use Custom JFrame component

--- JTestin.java
+++ JTestin.java
@@ -1,5 +1,5 @@
 public class JTestin{
   public static void main(String[] args){
-    System.out.println("helo");
-	}
+    MainFrame m = new MainFrame();
+  }
 }
---
+++ MainFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class MainFrame extends JFrame{
+    MainFrame(){
+        super();
+        this.setVisible(true);
+        this.setSize(800, 800);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
